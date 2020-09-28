3b1f96858b6127d67a5518b0d56ef5b434e16b21
Somers B Matthews <somersbmatthews@gmail.com>
2020-09-28 16:18:54 -0500

    finished 1.30

--- 1.30/1.30.go
+++ 1.30/1.30.go
@@ -1,9 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+var data = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+var search int = 6
+
+func main() {
+	result, index := BinarySearch(data, search)
+	fmt.Println(result, index)
 }
 
-func BinarySearch(data []int, search int) bool {
+func BinarySearch(data []int, search int) (bool, int) {
+	size := len(data)
+	var mid int
+	low := 0
+	high := size - 1
 
+	for low <= high {
+		mid = low + (high-low)/2
+		if data[mid] == search {
+			return true, mid
+		} else {
+			if data[mid] < search {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return false, 0
 }
---
+++ 1.30/1.30_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func ExampleBinarySearch() {
+	result, index := BinarySearch(data, search)
+	fmt.Println(result, index)
+	// Output:
+	// true 5
+}
